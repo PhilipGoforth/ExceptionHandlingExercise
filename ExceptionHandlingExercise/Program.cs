0adbb4b81766fd0c9de7e522c58a0de4e0fb4d50
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-16 14:40:46 -0600

    final

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -12,7 +12,9 @@ namespace ExceptionHandlingExercise
             // First create an char[], it must contain 6 numbers and 3 letters - name it arr
             // Create a list called numbers that will hold integers
             // Create an string variable with an empty string initializer - name it str
-
+            var arr = new char[] {'1','2','3','4','5','6','a','b','c' };
+            var numbers = new List<int>();
+            string str;
             // using a foreach loop, attempt to parse the elements in your char[] with int.Parse()
             // and Exceptions will be thrown 
             // Below we will set this up 
@@ -21,9 +23,21 @@ namespace ExceptionHandlingExercise
 
 
             //TODO START HERE:
-            
+
             // Make a foreach loop to iterate through your character array
-            
+            foreach (var item in arr)
+            {
+                try
+                {
+                    str = item.ToString();
+                    numbers.Add(Convert.ToInt32(str));
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine($"Unable to Parse '{item}'"); ;
+                }
+            }
                 // Now create a try catch
                 
                 
